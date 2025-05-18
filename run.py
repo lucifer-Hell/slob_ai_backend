539fe9043b9ab94618a5dd915a9c45a4f7201558
Pankaj Shukla - p0s0cad <57841799+lucifer-Hell@users.noreply.github.com>
2025-05-18 12:40:59 +0530

    5000 port change

--- run.py
+++ run.py
@@ -57,4 +57,4 @@ def predict_from_summary():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=7860, debug=False, use_reloader=False)
+    app.run(host="127.0.0.1", port=5000, debug=False, use_reloader=False)
